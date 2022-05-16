15ee492e20170dce8a7a9df69629708c8f6ddc90
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2022-05-16 21:17:02 +0530

    project files added

---
+++ Swift-UI Learn/Swift_UI_LearnApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_UI_LearnApp.swift
+//  Swift-UI Learn
+//
+//  Created by Hansa Anuradha on 2022-05-16.
+//
+
+import SwiftUI
+
+@main
+struct Swift_UI_LearnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
