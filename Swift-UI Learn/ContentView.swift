ec5a5509092488f5d2d9df5e151960d6e59163a6
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2022-05-18 23:11:41 +0530

    HeaderView added

--- Swift-UI Learn/ContentView.swift
+++ Swift-UI Learn/ContentView.swift
@@ -4,20 +4,12 @@ struct ContentView: View {
     var body: some View {
         
         VStack(spacing: 20) {
-            Text("Title")
-                .font(.largeTitle)
-            
-            Text("Subtitle")
-                .font(.title)
-                .foregroundColor(.gray)
-            
-            Text("Short description of what I'm demonstrating here")
-                .frame(maxWidth: .infinity)
-                .font(.title)
-                .foregroundColor(.white)
-                .padding()
-                .background(.blue)
+            HeaderView(title: "Title",
+                       subtitle: "Subtitle",
+                       description: "Short description of what I'm demonstrating here",
+                       background: .purple, textColor: .white)
         }
+        .font(.title)
         
     }
 }
---
+++ Swift-UI Learn/HeaderView.swift
@@ -0,0 +1,39 @@
+//
+//  HeaderView.swift
+//  Swift-UI Learn
+//
+//  Created by Hansa Anuradha on 2022-05-18.
+//
+
+import SwiftUI
+
+struct HeaderView: View {
+    
+    let title: String
+    let subtitle: String
+    let description: String
+    let background: Color
+    let textColor: Color
+
+    var body: some View {
+        
+        VStack(spacing: 20) {
+            Text(title)
+            
+            Text(subtitle)
+                .foregroundColor(.gray)
+            
+            Text(description)
+                .frame(maxWidth: .infinity)
+                .foregroundColor(textColor)
+                .padding()
+                .background(background)
+        }
+    }
+}
+
+struct HeaderView_Previews: PreviewProvider {
+    static var previews: some View {
+        HeaderView(title: "Title", subtitle: "Subtitle", description: "Short description of what I'm demonstrating here", background: .blue, textColor: .white)
+    }
+}
