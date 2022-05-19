97779fe1d292210313b3397c6319f0df34be86c9
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2022-05-19 22:06:05 +0530

    Previews

--- Swift-UI Learn/ContentView.swift
+++ Swift-UI Learn/ContentView.swift
@@ -4,27 +4,12 @@ struct ContentView: View {
     var body: some View {
         
         VStack(spacing: 20) {
-            HeaderView(title: "Layout Behavior",
-                       subtitle: "Views that Push Out",
-                       description: "Some views will push out to fill up all available space within their parent",
-                       background: .purple, textColor: .white)
+            HeaderView(title: "Previews",
+                       subtitle: "Introduction",
+                       description: "Xcode looks for a struct that conforms to the PreviewProvider protocol and accesses its previews property to display a view on the Canvas",
+                       background: .red, textColor: .white)
             
             
-            Color.purple
-                .overlay(Image(systemName: "arrow.up.left")
-                    .padding()
-                         , alignment: .topLeading)
-                .overlay(Image(systemName: "arrow.up.right")
-                    .padding()
-                         , alignment: .topTrailing)
-                .overlay(Image(systemName: "arrow.down.left")
-                    .padding()
-                         , alignment: .bottomLeading)
-                .overlay(Image(systemName: "arrow.down.right")
-                    .padding()
-                         , alignment: .bottomTrailing)
-                .overlay(Text("Colors are Push-Out views"))
-                .foregroundColor(.white)
 
         }
         .font(.title)
