d97262c182ae217bcb9d7b499d3d28faafa26b64
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2022-05-19 22:00:51 +0530

    Push-Out views

--- Swift-UI Learn/ContentView.swift
+++ Swift-UI Learn/ContentView.swift
@@ -5,25 +5,26 @@ struct ContentView: View {
         
         VStack(spacing: 20) {
             HeaderView(title: "Layout Behavior",
-                       subtitle: "Views that Pull In",
-                       description: "Some views minimize their frame size so it is only as big as the content within it",
+                       subtitle: "Views that Push Out",
+                       description: "Some views will push out to fill up all available space within their parent",
                        background: .purple, textColor: .white)
             
-            Image(systemName: "arrow.down.to.line.alt")
             
-            HStack {
-                Image(systemName: "arrow.right.to.line.alt")
-                Text("Text views pull in")
-                Image(systemName: "arrow.left.to.line.alt")
-            }
-            
-            Image(systemName: "arrow.up.to.line.alt")
-            
-            Text("Pull-In views tend to center themselves within their parent container view")
-                .frame(maxWidth: .infinity)
+            Color.purple
+                .overlay(Image(systemName: "arrow.up.left")
+                    .padding()
+                         , alignment: .topLeading)
+                .overlay(Image(systemName: "arrow.up.right")
+                    .padding()
+                         , alignment: .topTrailing)
+                .overlay(Image(systemName: "arrow.down.left")
+                    .padding()
+                         , alignment: .bottomLeading)
+                .overlay(Image(systemName: "arrow.down.right")
+                    .padding()
+                         , alignment: .bottomTrailing)
+                .overlay(Text("Colors are Push-Out views"))
                 .foregroundColor(.white)
-                .padding()
-                .background(.purple)
 
         }
         .font(.title)
@@ -33,6 +34,8 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+        }
     }
 }
