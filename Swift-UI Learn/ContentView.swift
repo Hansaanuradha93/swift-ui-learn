0078f98631142503c40aaf27583fe757b423d096
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2022-05-20 23:22:10 +0530

    Nesting VStacks

--- Swift-UI Learn/ContentView.swift
+++ Swift-UI Learn/ContentView.swift
@@ -5,19 +5,30 @@ struct ContentView: View {
         
         VStack(spacing: 20) {
             HeaderView(title: "VStacks",
-                       subtitle: "Introduction",
-                       description: "A VStack will vertically arrange views within it.",
+                       subtitle: "Nesting",
+                       description: "A VStack can be nested within another VStack when laying out views",
                        background: .blue, textColor: .white)
             
-            Text("Text 1")
-            Text("Text 2")
-            Text("Text 3")
-            Text("Text 4")
-            Text("Text 5")
-            Text("Text 6")
-            Text("Text 7")
-            Text("Text 8")
-            Text("Text 9")
+            VStack {
+                Text("VStack inside another VStack")
+                
+                Divider()
+                
+                Text("This can be helpful, Why?")
+
+                Divider()
+                
+                Text("More than 10 views creates an error")
+                
+            }
+            .padding()
+            .foregroundColor(.white)
+            .background(
+                RoundedRectangle(cornerRadius: 20)
+                    .foregroundColor(.blue)
+            )
+            .padding()
+            
 
 
         }
