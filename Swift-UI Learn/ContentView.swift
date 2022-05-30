0ab9502171b6acb66495a35052cdd6469d3d62f8
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2022-05-30 22:24:30 +0530

    code cleaned

--- Swift-UI Learn/ContentView.swift
+++ Swift-UI Learn/ContentView.swift
@@ -4,9 +4,9 @@ struct ContentView: View {
     var body: some View {
         
         VStack(spacing: 40) {
-            HeaderView(title: "HStacks",
-                       subtitle: "First & Last Text Alignment",
-                       description: "The firstTestBaseLine will align the bottom fo the text on the first lines (\"Amazing\" and \"Really\")",
+            HeaderView(title: "",
+                       subtitle: "",
+                       description: "",
                        background: .orange, textColor: .black)
             
 
