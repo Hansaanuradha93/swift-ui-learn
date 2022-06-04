2ed42d3d4c6168def4b5dfb4de4d548bf61e938e
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2022-06-04 12:19:33 +0530

    HStack -> Exercise 1

--- Swift-UI Learn/ContentView.swift
+++ Swift-UI Learn/ContentView.swift
@@ -4,10 +4,16 @@ struct ContentView: View {
     var body: some View {
         
         VStack(spacing: 40) {
-            HeaderView(title: "",
-                       subtitle: "",
-                       description: "",
+            HeaderView(title: "HStack",
+                       subtitle: "Excersice",
+                       description: "Test",
                        background: .orange, textColor: .black)
+            
+            HStack (spacing: 20) {
+                Text("Test 1")
+                Text("Test 2")
+                Text("Test 3")
+            }
         }
         
     }
