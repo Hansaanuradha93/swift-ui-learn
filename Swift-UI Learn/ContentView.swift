22d3c026348ffe95447e9c7137b8e4405de3e57a
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2022-06-19 21:20:11 +0530

    code cleaned

--- Swift-UI Learn/ContentView.swift
+++ Swift-UI Learn/ContentView.swift
@@ -8,21 +8,6 @@ struct ContentView: View {
                        subtitle: "Excersice",
                        description: "Test",
                        background: .orange, textColor: .black)
-            
-            VStack(spacing: 20) {
-                Image(systemName: "arrow.up.square.fill")
-                
-                HStack(spacing: 20) {
-                    Image(systemName: "arrow.left.square.fill")
-                    Spacer()
-                        .frame(maxWidth: 50)
-                    Image(systemName: "arrow.right.square.fill")
-                }
-
-                
-                Image(systemName: "arrow.down.square.fill")
-            }
-            .font(.largeTitle)
 
         }
          
