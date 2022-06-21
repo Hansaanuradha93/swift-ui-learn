098d291d9e3302480ca04b940af3694bd7dfe2cd
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2022-06-21 22:30:16 +0530

    HStack -> exercise 4

--- Swift-UI Learn/ContentView.swift
+++ Swift-UI Learn/ContentView.swift
@@ -8,6 +8,10 @@ struct ContentView: View {
                        subtitle: "Excersice",
                        description: "Test",
                        background: .orange, textColor: .black)
+            
+            HStack {
+                Text("I can talk to my remote")
+            }
 
         }
          
