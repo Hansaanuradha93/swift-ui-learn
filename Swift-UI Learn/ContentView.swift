d80f3cefdccd764890b64bc9cd74351a52c4ecc2
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2022-07-10 21:44:31 +0530

    code cleaned

--- Swift-UI Learn/ContentView.swift
+++ Swift-UI Learn/ContentView.swift
@@ -9,7 +9,6 @@ struct ContentView: View {
                        description: "Test",
                        background: .orange, textColor: .black)
             
-            
         }
          
     }
