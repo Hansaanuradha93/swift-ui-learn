e4ca59f39e822972367f792c563140f4722cec58
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2022-07-14 19:47:45 +0530

    HStacks -> Customising

--- Swift-UI Learn/ContentView.swift
+++ Swift-UI Learn/ContentView.swift
@@ -5,14 +5,39 @@ struct ContentView: View {
         
         VStack(spacing: 40) {
             HeaderView(title: "HStack",
-                       subtitle: "Excersice",
-                       description: "Test",
+                       subtitle: "Customizing",
+                       description: "HStacks are views that can have modifiers applied to them just like any other view",
                        background: .orange, textColor: .black)
             
-            VStack {
-                Text("Hansa")
-                Text("Anuradha")
+            HStack {
+                Text("Leading")
+                Text("Middle")
+                Text("Trailing")
             }
+            .padding()
+            .border(Color.orange)
+            
+            HStack (spacing: 10) {
+                Image(systemName: "1.circle")
+                Image(systemName: "2.circle")
+                Image(systemName: "3.circle")
+            }
+            .padding()
+            
+            HStack (spacing: 20) {
+                Image(systemName: "a.circle.fill")
+                Image(systemName: "b.circle.fill")
+                Image(systemName: "c.circle.fill")
+                Image(systemName: "d.circle.fill")
+                Image(systemName: "e.circle.fill")
+            }
+            .font(.largeTitle)
+            .padding()
+            .background(Color.orange)
+            .cornerRadius(10)
+            
+            
+            
         }
          
     }
