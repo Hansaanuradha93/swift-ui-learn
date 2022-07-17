1ef74572b1f5a2801051b55b01afdaae2fad51d5
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2022-07-17 22:03:04 +0530

    code cleaned

--- Swift-UI Learn/ContentView.swift
+++ Swift-UI Learn/ContentView.swift
@@ -9,29 +9,7 @@ struct ContentView: View {
                        description: "Use the layoutPriority modifier to give priority to the space a view needs to show its content",
                        background: .orange, textColor: .black)
             
-            HStack {
-                Text("SwiftUI")
-                    .font(.largeTitle)
-                    .lineLimit(1)
-                Image("SwiftUI")
-                    .resizable()
-                    .frame(width: 80, height: 80)
-                Text("Brings Balance")
-                    .font(.largeTitle)
-                    .layoutPriority(1)
-            }
             
-            HStack {
-                Text("SwiftUI")
-                    .font(.largeTitle)
-                    .layoutPriority(1)
-                Image("SwiftUI")
-                    .resizable()
-                    .frame(width: 80, height: 80)
-                Text("Brings Balance")
-                    .font(.largeTitle)
-                    .lineLimit(1)
-            }
             
         }
          
